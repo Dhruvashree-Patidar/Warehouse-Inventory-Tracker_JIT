0d92989edffcc75da14b79cbb3dd1f22c55a525e
Dhruvashree-Patidar <dhruvashree5patidar@gmail.com>
2025-10-22 17:30:15 +0530

    Update StockAlertService.java

--- Warehouse_Inventory_Tracker/src/StockAlertService.java
+++ Warehouse_Inventory_Tracker/src/StockAlertService.java
@@ -5,8 +5,9 @@ public class StockAlertService implements StockObserver
 	@Override
 	public void onLowStock(Product p) 
 	{
-		System.out.println("⚠️ ALERT: Low stock for " + p.getName() 
+		System.out.println("ALERT: Low stock for " + p.getName() 
         + " - Only " + p.getQuantity() + " left!");
 		
 	}
 }
+
