606ff2f5a001e89f7e59239391966d947cc3b1c5
Dhruvashree-Patidar <dhruvashree5patidar@gmail.com>
2025-10-31 15:36:32 +0530

    Update Main.java

--- Warehouse_Inventory_Tracker/src/Main.java
+++ Warehouse_Inventory_Tracker/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main 
@@ -5,22 +6,21 @@ public class Main
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
-
+        
         StockObserver alertService = new StockAlertService();
-        Warehouse warehouse = new Warehouse(alertService);
 
-        warehouse.loadFromFile("inventory.txt");
+        save s=new save();
+        HashMap<Integer, Warehouse> warehouses = s.loadDataFromFile(alertService);
 
         int choice;
         do 
         {
-            System.out.println("\n--- Warehouse Inventory Menu ---");
-            System.out.println("1. Add Product");
-            System.out.println("2. Receive Shipment");
-            System.out.println("3. Fulfill Order");
-            System.out.println("4. Display All Products");
-            System.out.println("5. Exit");
-            System.out.print("Enter your choice: ");
+        	System.out.println("1. Add Warehouse");
+        	System.out.println("2. Add Product to Warehouse");
+        	System.out.println("3. Receive Shipment");
+        	System.out.println("4. Fulfill Order");
+        	System.out.println("5. Display Warehouse Inventory");
+        	System.out.println("6. Exit");
             
             while (!sc.hasNextInt()) 
             { 
@@ -32,57 +32,125 @@ public class Main
             switch (choice) 
             {
                 case 1:
-                    System.out.print("Enter Product ID: ");
-                    int id = sc.nextInt();
-                    sc.nextLine(); 
-                    
-                    System.out.print("Enter Product Name: ");
-                    String name = sc.nextLine();
-                    
-                    System.out.print("Enter Initial Quantity: ");
-                    int qty = sc.nextInt();
-                    
-                    System.out.print("Enter Minimum Quantity (Threshold): ");
-                    int minQty = sc.nextInt();
-                    
-                    Product p = new Product(id, name, qty, minQty);
-                    warehouse.add_Product(p);
-                    break;
-
+                	System.out.print("Enter Warehouse ID: ");
+                	int wId = sc.nextInt();
+                	sc.nextLine();
+                	System.out.print("Enter Warehouse Name: ");
+                	String wName = sc.nextLine();
+                	if (warehouses.containsKey(wId)) 
+                	{
+                		System.out.println("already exists! Cannot add duplicate");
+                	}
+                	else
+                	{
+	                	Warehouse w = new Warehouse(wId, wName, alertService);
+	                	warehouses.put(wId, w);
+	                	System.out.println("Warehouse added successfully!");
+                	}
+                	break;
+                	
+                	
+                	
                 case 2:
-                    System.out.print("Enter Product ID: ");
-                    int sId = sc.nextInt();
-                    
-                    System.out.print("Enter Quantity to Add: ");
-                    int addQty = sc.nextInt();
-                    
-                    warehouse.receive_Shipment(sId, addQty);
+                	System.out.print("Enter Warehouse ID: ");
+                	int wid = sc.nextInt();
+
+                	if (warehouses.containsKey(wid)) 
+                	{
+                	    Warehouse selected = warehouses.get(wid);
+
+                	    System.out.print("Enter Product ID: ");
+                	    int pId = sc.nextInt();
+                	    sc.nextLine();
+                	    System.out.print("Enter Product Name: ");
+                	    String pName = sc.nextLine();
+                	    System.out.print("Enter Quantity: ");
+                	    int qty = sc.nextInt();
+                	    System.out.print("Enter Min Quantity: ");
+                	    int minQty = sc.nextInt();
+
+                	    Product p = new Product(pId, pName, qty, minQty);
+                	    selected.add_Product(p);
+
+                	} 
+                	else 
+                	{
+                	    System.out.println("Warehouse not found!");
+                	}
+
                     break;
 
                 case 3:
-                    System.out.print("Enter Product ID: ");
-                    int oId = sc.nextInt();
-                    
-                    System.out.print("Enter Quantity to Remove: ");
-                    int removeQty = sc.nextInt();
-                    
-                    warehouse.fulfill_Order(oId, removeQty);
+                	System.out.print("Enter Warehouse ID: ");
+                	int Id = sc.nextInt();
+
+                	if (warehouses.containsKey(Id)) 
+                	{
+                	    Warehouse selected = warehouses.get(Id);
+                	    
+                	    System.out.print("Enter Product ID: ");
+                        int sId = sc.nextInt();
+                        
+                        System.out.print("Enter Quantity to Add: ");
+                        int addQty = sc.nextInt();
+                	    selected.receive_Shipment(sId, addQty);
+                	}
+                	else 
+                	{
+                	    System.out.println("Warehouse not found!");
+                	}
+                	
                     break;
 
                 case 4:
-                    warehouse.display_All_Products();
+                	System.out.print("Enter Warehouse ID: ");
+                	int id = sc.nextInt();
+
+                	if (warehouses.containsKey(id)) 
+                	{
+                	    Warehouse selected = warehouses.get(id);
+                	    
+                	    System.out.print("Enter Product ID: ");
+                	    int oId = sc.nextInt();
+                    
+                	    System.out.print("Enter Quantity to Remove: ");
+                	    int removeQty = sc.nextInt();
+                    
+                	    selected.fulfill_Order(oId, removeQty);
+                	}
+                	else 
+                	{
+                	    System.out.println("Warehouse not found!");
+                	}
                     break;
 
                 case 5:
-                    System.out.println("Saving inventory and exiting...");
-                    warehouse.saveToFile("inventory.txt");
+                	System.out.print("Enter Warehouse ID: ");
+                	int ID = sc.nextInt();
+
+                	if (warehouses.containsKey(ID)) 
+                	{
+                	    Warehouse selected = warehouses.get(ID);
+                	    selected.display_All_Products();
+                	}
+                	else 
+                	{
+                	    System.out.println("Warehouse not found!");
+                	}
+                    break;
+
+                case 6:
+                	
+                	    System.out.println("Saving inventory and exiting...");
+                	    s.saveDataToFile(warehouses);
+ 
                     break;
 
                 default:
                     System.out.println("Invalid choice! Try again.");
             }
         } 
-        while (choice != 5);
+        while (choice != 6);
 
         sc.close();
     }
